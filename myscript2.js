f4974fb4393df575865297bcb2783285a11455d6
Shinichi Nozawa <llshinichill@yahoo.co.jp>
2014-02-21 23:46:04 +0900

    sessionStrageに保存
     - 別ファイルを作成
     - ToDoをsessionStorageに保存する版
     - デザインは本家と同じ
     - nozawashinichi.sakura.ne.jp/cgi-samples/todo/todo.html にデモ

---
+++ myscript2.js
@@ -0,0 +1,58 @@
+var mainCtrl = function($scope, $http) {
+	// { "body": "hogehoge", "done": false}
+	
+	var storage = sessionStorage;
+	var time = new Date;
+	//console.log( storage.getItem("tasksStr") );
+	var tasksStr = "tasksStr" + time;
+	var doneTasksStr = "doneTasksStr" + time;
+
+	if(storage.getItem(tasksStr)){
+		$scope.tasks = JSON.parse( storage.getItem(tasksStr) );	
+	}else{
+		$scope.tasks = [{"body":"あれやらなきゃ", "done":false}, {"body":"これやらなきゃ", "done":false}, {"body":"やった", "done":true}];
+	}
+	if(storage.getItem("doneTasksStr")){
+		$scope.doneTasks = JSON.parse( storage.getItem(doneTasksStr) );	
+	}else{
+		$scope.doneTasks = [];
+	}
+
+	$scope.addNew = function(){
+		if($scope.newTaskBody == ''){
+			return;
+		}
+		$scope.tasks.push({ "body": $scope.newTaskBody, "done": false});
+		$scope.newTaskBody = '';
+		storage.setItem("tasksStr", JSON.stringify($scope.tasks) );
+	}
+
+	$scope.getDoneCount = function(){
+		var count = 0;
+		angular.forEach($scope.tasks, function(task){
+			count += task.done ? 1 : 0;
+		});
+		return count;
+	}
+
+	$scope.getProgress = function(){
+		var count = $scope.getDoneCount();
+		var percent = parseInt( ( count / $scope.tasks.length ) * 100 );
+		return percent;
+	}
+
+	$scope.deleteDone = function(){
+		var oldTasks = $scope.tasks;
+		$scope.tasks = [];
+		angular.forEach(oldTasks, function(task){
+			if(!task.done){
+				$scope.tasks.push(task);				
+			}else{
+				task.doneDate = new Date();
+				$scope.doneTasks.unshift(task);
+			}
+		});
+		storage.setItem("tasksStr", JSON.stringify($scope.tasks) );
+		storage.setItem("doneTasksStr", JSON.stringify($scope.doneTasks) );
+	}
+}
